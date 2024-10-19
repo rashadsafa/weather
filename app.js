4e97b8424ce57b242662056c5004a10642351482
rashadsafa rashadrashad <rashad257rs@mail.ru>
2024-10-19 18:54:06 +0000

    weather

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url 
+            let url =
+                async function myweather() {
+                    let responce = await fetch(url)
+                    let data = await responce.json()
+                    $("#city").html(data.name)
+                    $("#temp").html(data.main.temp)
+                    $("#feels").html(data.main.feels_like)
+                    $("#myweather").val("")
+                    console.log(data);
+                }
+            myweather()
         }
     })
+
+    $("#btn").click(function (e) {
+        e.preventDefault();
+        let url =
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myweather").val("")
+                console.log(data);
+            }
+        myweather()
+
+    });
 });
