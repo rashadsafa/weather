fb394003c09e8c29eb4cd7048af40c105c098375
rashadsafa rashadrashad <rashad257rs@mail.ru>
2024-10-21 18:27:16 +0000

    try

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 "use strict";
 $(document).ready(function () {
 
+
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&APPID=8dac6093e79631caef3049d1a566ba33`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&APPID=8dac6093e79631caef3049d1a566ba33`;
             async function myweather() {
-                let responce = await fetch(url)
-                let data = await responce.json()
-                $("#city").html(data.name)
-                $("#temp").html(data.main.temp)
-                $("#feels").html(data.main.feels_like)
-                $("#myweather").val("")
-                console.log(data);
+                let responce = await fetch(url);
+                let data = await responce.json();
+                $("#city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("#myWeather").val("");
+                console.log(data)
             }
             myweather()
         }
-    })
+
+    });
 
     $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&APPID=8dac6093e79631caef3049d1a566ba33`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&APPID=8dac6093e79631caef3049d1a566ba33`;
         async function myweather() {
-            let responce = await fetch(url)
-            let data = await responce.json()
-            $("#city").html(data.name)
-            $("#temp").html(data.main.temp)
-            $("#feels").html(data.main.feels_like)
-            $("#myweather").val("")
-            console.log(data);
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myWeather").val("");
+            console.log(data)
         }
         myweather()
-
     });
+
+    
 });
