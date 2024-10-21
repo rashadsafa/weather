dd13d9ebe0a6783a055d9629b006d9e8261155af
rashadsafa rashadrashad <rashad257rs@mail.ru>
2024-10-21 19:22:49 +0000

    whole

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&APPID=8dac6093e79631caef3049d1a566ba33`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&APPID=8dac6093e79631caef3049d1a566ba33&units=metric`;
             async function myweather() {
                 let responce = await fetch(url);
                 let data = await responce.json();
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&APPID=8dac6093e79631caef3049d1a566ba33`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&APPID=8dac6093e79631caef3049d1a566ba33&units=metric`;
         async function myweather() {
             let responce = await fetch(url);
             let data = await responce.json();
